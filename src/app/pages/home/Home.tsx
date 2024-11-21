63c263ea137d52341b906b71dc5c50279f36b96e
Lauriano <laurianocarlos@hotmail.com>
2024-11-20 21:29:13 -0300

    Fix:Home

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import Layout from '../../pages/Layout/Layout';
 import FolderList from '../../components/Folders/FolderList';
 
 const Home: React.FC = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
   return (
     <Layout title="Bem-vindo!">
       <FolderList />
     </Layout>
   );
 };
+
 export default Home;
